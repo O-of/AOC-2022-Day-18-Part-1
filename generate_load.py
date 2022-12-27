1226b8dc2ebd7a999c5e92bad4fbec508c7144a9
O-of <78329344+O-of@users.noreply.github.com>
2022-12-26 22:53:57 -0500

    fix load file

--- generate_load.py
+++ generate_load.py
@@ -2,4 +2,5 @@ with open("input.txt") as f:
     a = f.read().split("\n")
 
 with open("load.mcfunction", "w") as f:
+    f.write("".join(f"fill {x} {y} {z} {x+50} {y+10} {z+10} air\n" for z in (0, 50) for y in range(0, 100, 10) for x in (0, 50)))
     f.write("\n".join(f"setblock {i.replace(',', ' ')} minecraft:iron_block" for i in a))
